6f0b77c16ed7811e48ade2831af9f7d280c8eb5a
JyothinagaramMeghana <meghasetty1@gmail.com>
2025-08-26 14:20:45 +0530

    fixed Feature Request: Forgot Password Functionality

--- nouproject/settings.py
+++ nouproject/settings.py
@@ -128,7 +128,8 @@ MEDIA_LOOP=os.path.join(BASE_DIR,'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# Email backend for development (reset link will print in terminal)
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
